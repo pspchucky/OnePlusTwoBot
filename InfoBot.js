4ee985920f65de235826db4c07cf873332d8ba77
Sergio German <pspchucky@users.noreply.github.com>
2015-08-15 02:19:14 -0700

    Some semi-colons here and there

--- InfoBot.js
+++ InfoBot.js
@@ -1,9 +1,9 @@
 var colour = require('colors'); //Muahaha useless but fun to play with
 var https = require('https');
-var kid = "PG2FQO" //Pulled someone's 'kid' ('k-id', not kid >:|) off twitter (via https://twitter.com/balkumar/status/632149668726607872) Use your own... (or be malicious, I take no responibility...)
+var kid = "PG2FQO"; //Pulled someone's 'kid' ('k-id', not kid >:|) off twitter (via https://twitter.com/balkumar/status/632149668726607872) Use your own... (or be malicious, I take no responibility...)
 var json = ''; //Prepare the json response, might come in chunks...
 
-console.log("Fetching ".yellow + kid.inverse + "'s status in the system...".yellow)
+console.log("Fetching ".yellow + kid.inverse + "'s status in the system...".yellow);
 
 https.get('https://invites.oneplus.net/index.php?r=share/view&kid=' + kid, function(res) {
   res.on('data', function(chunk) {
@@ -11,17 +11,17 @@ https.get('https://invites.oneplus.net/index.php?r=share/view&kid=' + kid, funct
   });
 
   res.on('end', function() {
-    var jRes = JSON.parse(json)
-    var jData = jRes.data
+    var jRes = JSON.parse(json);
+    var jData = jRes.data;
     console.log("==================================================".rainbow); //I had to
     console.log((jData.kid).inverse + " has ".yellow + (jData.ref_count).toString().inverse + " refferals total.".yellow); //Refferals
     console.log("and has ".yellow + (jData.credits).toString().inverse + " 'credits' (whatever that means...)".yellow); //Credits ?
     console.log("and is ranked @ ".yellow + (numberWithCommas(jData.rank)).inverse + " out of a total of ".yellow + (numberWithCommas(jData.total)).inverse + " others".yellow); //Rank + Total
     console.log("and was created on ".yellow + (timeConverter(jData.create_at)).inverse ); //When the kid registered
     if (jData.parent != ""){ console.log("and their 'parent' is ".yellow + (jData.parent).red ); } //Check if the 'kid' has a 'parent' (uh)
-    if (jData.referer != undefined){ console.log("and their 'referer' was '".yellow + (jData.referer).inverse + "'".yellow); } //Check if the 'kid' was reffered by an external source
-    if (jData.ip != undefined){ console.log("and their IP address is ".yellow + (jData.ip).red + " (security flaw?)".red); } // silly 1+
-    if (jData.userIp != undefined){ console.log("and their IP address is ".yellow + (jData.userIp).red + " (security flaw?)".red); } // 1+ pls
+    if (jData.referer !== undefined){ console.log("and their 'referer' was '".yellow + (jData.referer).inverse + "'".yellow); } //Check if the 'kid' was reffered by an external source
+    if (jData.ip !== undefined){ console.log("and their IP address is ".yellow + (jData.ip).red + " (security flaw?)".red); } // silly 1+
+    if (jData.userIp !== undefined){ console.log("and their IP address is ".yellow + (jData.userIp).red + " (security flaw?)".red); } // 1+ pls
     console.log("==================================================".rainbow); //This project is for fun ok?
   });
 
